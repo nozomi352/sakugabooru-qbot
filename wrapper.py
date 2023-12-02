e9b6e97183519d6d45533a06326c9cf70587ea2f
nozomi <1569682531@qq.com>
2023-12-02 15:06:32 +0800

    add help and xiongwen; 雄文功能1t4

--- wrapper.py
+++ wrapper.py
@@ -36,6 +36,10 @@ class InputMessageWrapper:
             return self.handle_greetings()
         elif self.message.startswith('.post '):
             return await self.handle_sakuga_booru_post()
+        elif self.message.startswith('.xiongwen '):
+            return await self.handle_text_generator()
+        elif self.message.startswith('.help'):
+            return await self.hendle_help()
         else:
             return None
 
@@ -71,3 +75,46 @@ class InputMessageWrapper:
         weibo_img_url = j['data']['file']
         print('file url:', weibo_img_url)
         return self.reply_message(f'{post_id}\n[CQ:image,file=file:///{weibo_img_url}] {display_tags}')
+    
+    async def handle_text_generator (self):
+        text = self.message.split('.xiongwen ')[1]
+        anime = text.split(' ')[0]
+        keywords = text.split(' ')[1:]
+        if not anime or len(keywords) == 0:
+            return self.reply_message('请求格式错误')
+        print('anime:', anime)
+        print('keywords:', keywords)
+        with open('gpt-api-key.txt', 'r', encoding='utf-8') as f:
+            api_key = f.read()
+        async with aiohttp.ClientSession() as session:
+            session.headers.update({'Authorization': 'Bearer ' + api_key})
+            data = {
+                'model': 'gpt-3.5-turbo',
+                'messages': [
+                    {
+                        'role': 'user',
+                        'content': f'以{"、".join(keywords)}为关键词, 生成一篇评价动漫《{anime}》的文章，300字左右'
+                    }
+                ],
+                "temperature": 0.7
+            }
+            async with session.post('https://api.chatanywhere.com.cn/v1/chat/completions', json=data) as r:
+                if r.status != 200:
+                    return self.reply_message('无法调用AI API')
+                j = await r.json()
+        choices = j['choices']
+        if not choices or len(choices) == 0:
+            return self.reply_message('AI也不知道怎么回答哦')
+        choice = choices[0]
+        text = choice['message']['content']
+        return self.reply_message(text)
+
+
+    async def hendle_help (self):
+        print('handle help')
+        return self.reply_message('''
+.echo <message> - 回复 <message>
+.post <id> - 发布sakugabooru上的动画截图
+.xiongwen <anime> <keyword0>  <keyword1> ... - 使用<keyword>作为关键词, 生成一篇关于<anime>的文章
+        ''')
+
